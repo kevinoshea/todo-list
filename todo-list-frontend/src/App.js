b6c558dc8adeb907d8c23785adbbff9d066dfa4c
Kevin O'Shea <kevinoshea.mail@gmail.com>
2025-01-24 21:53:32 +1100

    Add frontend application scaffolding

---
+++ todo-list-frontend/src/App.js
@@ -0,0 +1,11 @@
+import ToDoList from './modules/ToDoList';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list-frontend/src/modules/ToDoList.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const ToDoList: FC = () => {
+  return (
+    <div>
+      <h1>TO DO</h1>
+    </div>
+  );
+};
+
+export default ToDoList;
